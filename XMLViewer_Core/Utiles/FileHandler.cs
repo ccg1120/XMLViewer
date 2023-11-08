4cfb85879bfc7f01d81088583b48de675b0877ac
ccg1120 <ccg1120@naver.com>
2023-11-08 13:56:57 +0900

    라이브러리 용 솔루션 추가 파일로드 기능 추가 확장성있도록 수정 필요

---
+++ XMLViewer_Core/Utiles/FileHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLViewer_Core.Utiles
+{
+    enum XMLFileLoadReturnValue
+    {
+        Success,
+        ExistsFile,
+        NoXMLExtention,
+        NullFilePath
+    }
+
+    /// <summary>
+    /// 파일을 읽어서 값을 전달 하는 역할
+    /// </summary>
+    internal class FileHandler
+    {
+        static XMLFileLoadReturnValue LoadXMLFile(ref string filePath, ref string readOutput)
+        {
+            if ( string.IsNullOrEmpty(filePath))
+            {
+                return XMLFileLoadReturnValue.NullFilePath;
+            }
+
+            FileInfo fileInfo = new FileInfo(filePath);
+            if(fileInfo.Exists == false)
+            {
+                return XMLFileLoadReturnValue.ExistsFile;
+            }
+            if (fileInfo.Extension.Normalize().Equals(".xml") == false)
+            {
+                return XMLFileLoadReturnValue.NoXMLExtention;
+            }
+
+            readOutput = File.ReadAllText(filePath, Encoding.UTF8);
+            return XMLFileLoadReturnValue.Success;
+        }
+    }
+}
