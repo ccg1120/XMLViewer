bebbbb0f92bbcc759be3abf834f74343652ce870
ccg1120 <ccg1120@naver.com>
2023-11-10 13:50:56 +0900

    테스트 데이터 제작용 코스 작성중

---
+++ FunctionTest/TestDataExporter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FunctionTest
+{
+    internal class TestDataExporter
+    {
+        //테스트 파일 제작 코드
+        // 폴더와 파일을 기준으로 노드를 만듬
+        public bool CreateTextXMLData(in string path, string exportPath)
+        {
+            DirectoryInfo rootDirectory = new DirectoryInfo(path);
+            XmlDocument xmlDocument = new XmlDocument();
+            Parser(xmlDocument, rootDirectory);
+            return true;
+        }
+
+        private void Parser(XmlDocument xml, DirectoryInfo rootFolder)
+        {
+            DirectoryInfo[] directoryInfos = rootFolder.GetDirectories();
+            foreach (DirectoryInfo directoryInfo in directoryInfos)
+            {
+                string name = directoryInfo.Name;
+                string createTime = directoryInfo.CreationTime.ToString();
+                string path = directoryInfo.FullName;
+                string fileCount = directoryInfo.GetFiles().Count().ToString();
+            }
+            FileInfo[] files = rootFolder.GetFiles();
+        }
+    }
+}
