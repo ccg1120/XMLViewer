c236a809ec6edd5781838fc68018d73bd7826a18
PC\ccg11 <ccg1120@naver.com>
2023-11-27 00:45:20 +0900

    로거 작업 추가 기능확인 해야함

--- XMLViewer_Core/Utiles/Logger.cs
+++ XMLViewer_Core/Utiles/Logger.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,48 +11,68 @@ namespace XMLViewer_Core.Utiles
 {
     public enum LogLevel
     {
+        Dev,
         Info,
         Warn,
         Error
     }
 
-    public class Logger
+    public class Logger : IDisposable
     {
-        private static Logger instance = new Logger();
+        public static void Log(LogLevel logLevel, string msg)
+        {
+            if( _instance._isInitailizeLogger == false)
+                _instance.initalizeLogger();
+
+            if ( (logLevel == LogLevel.Dev) && (_instance._isDebuggerAttached == false) )
+                return;
+
+            string logString = $"LogLevel[{logLevel.ToString()}]:: {msg}";
+            Console.WriteLine(logString);
+            _instance._streamWriter.WriteLine(logString);
+        }
+
+        public void Dispose()
+        {
+            if (_streamWriter.Equals(StreamWriter.Null) == false)
+                _streamWriter.Dispose();
+        }
+
         private Logger()
         {
+            _streamWriter = StreamWriter.Null;
+            _isDebuggerAttached = Debugger.IsAttached;
+        }
+        private void initalizeLogger()
+        {
             string logFileSavePath = getLogFilePath();
-
-            if(File.Exists(logFileSavePath) == false)
+            if (File.Exists(logFileSavePath) == false)
             {
                 try
                 {
-                    using(File.CreateText( logFileSavePath))
-                    {
-
-                    }
-
+                    _streamWriter = File.CreateText(logFileSavePath);
+                    _streamWriter.WriteLine("initalizeLogger");
                 }
                 catch (Exception e)
                 {
-
+                    Console.WriteLine(e.ToString());
+                    if (_streamWriter.Equals(StreamWriter.Null) == false)
+                        _streamWriter.Dispose();
                 }
             }
+            _isInitailizeLogger = true;
         }
-
-        public static void Log(LogLevel logLevel, string msg)
-        {
-            string logString = $"LogLevel[{logLevel.ToString()}]:: {msg}";
-            Console.WriteLine(logString);
-            Console.WriteLine(logLevel);
-        }
-
-        private static string getLogFilePath()
+        private string getLogFilePath()
         {
             DateTime currentDateTime = DateTime.Now;
             string timeFormat = "yyyyMMdd_HHmmss";
             string time = currentDateTime.ToString(timeFormat);
-            return Path.Combine(Directory.GetCurrentDirectory(), "XMLViewer_", time,".log");
+            return Path.Combine(Directory.GetCurrentDirectory(), "XMLVi`ewer_", time, ".log");
         }
+
+        private static Logger       _instance = new Logger();
+        private bool                _isInitailizeLogger;
+        private StreamWriter        _streamWriter;
+        private readonly bool       _isDebuggerAttached;
     }
 }
